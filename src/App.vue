64c8a0049fc3947dbfd07298a2061f5f7ebcbc37
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-05 20:40:55 +0700

    add more coin

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <h3>status: {{ status }}</h3>
   <button @click='toss'>Toss</button>
+  <button @click='add'>Add Coin</button>
+  <button @click='remove'>Remove Coin</button>
 </template>
 
 <script>
@@ -8,13 +10,22 @@ export default {
   name: 'App',
   data() {
     return {
-      status: 'H'
+      status: ['H']
     }
   },
   methods: {
     toss() {
       const hOrT = 'HT';
-      this.status = hOrT[Math.floor(Math.random() * 2)];
+      // this.status = hOrT[Math.floor(Math.random() * 2)];
+      for (let i in this.status){
+        this.status[i] = hOrT[Math.floor(Math.random() * 2)];
+      }
+    },
+    add() {
+      this.status.push('H');
+    },
+    remove() {
+      this.status.pop();
     }
   }
 }
